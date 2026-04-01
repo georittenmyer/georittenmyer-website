dbb701fc672aeafc33e3b3ffe7c7d99c5a3529bf
georittenmyer <geo.rittenmyer@gmail.com>
2026-04-01 13:36:44 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,9 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
   site: 'https://georittenmyer.com',
   integrations: [sitemap()],
+  vite: {
+    build: {
+      minify: 'terser'
+    }
+  }
 });
